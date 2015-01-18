1576517f55e5fb4c0e8b51fda0c35e96e6538762
David Gowers <finticemo@gmail.com>
2015-01-18 14:25:08 +1030

    config.def.h: Cleanup keybinds, avoiding alphanumeric, C-alphanumeric, and F1-F12 CS-alphanumeric is permitted (and used). For example CS-s to start slideshow, CS-a to toggle antialiasing Marking commands are now associated with Menu key rather than 'm'

--- config.def.h
+++ config.def.h
@@ -33,7 +33,7 @@ static const float zoom_levels[] = {
 	1600.0, 2400.0
 };
 
-/* default slideshow delay (in msec, overwritten via -S option): */
+/* default slideshow delay (in 10th of a second, overwritten via -S option): */
 /* this is 90 seconds / 1m30s */
 enum { SLIDESHOW_DELAY = 900 };
 
@@ -82,28 +82,21 @@ static const int THUMBNAIL_PIXELIZE_AT = 201;
 #ifdef _MAPPINGS_CONFIG
 
 /* keyboard mappings for image and thumbnail mode: */
-/* Still needs simplification. "There should be one and preferably only one way to do it." */
+/* Plain Alphanumerics, shifted alphanumerics, control+alphanumerics and function keys are explicitly avoided,
+   to allow the key handler script maximum latitude. */
 static const keymap_t keys[] = {
 	/* modifiers    key               function              argument */
-        { 0,            XK_q,             g_quit,               None },
+        { ShiftMask,    XK_Escape,        g_quit,               None },
         { 0,            XK_Return,        g_switch_mode,        None },
-        { 0,            XK_f,             g_toggle_fullscreen,  None },
-        { 0,            XK_b,             g_toggle_bar,         None },
-//	{ 0,            XK_plus,          g_zoom,               +1 },
+        { ControlMask,  XK_F,             g_toggle_fullscreen,  None },
+        { ControlMask,  XK_B,             g_toggle_bar,         None },
 	{ 0,            XK_KP_Add,        g_zoom,               +1 },
-//	{ 0,            XK_minus,         g_zoom,               -1 },
 	{ 0,            XK_KP_Subtract,   g_zoom,               -1 },
-	{ 0,            XK_R,             t_reload_all,         None },
-	{ ShiftMask,    XK_F5,            t_reload_all,         None },
+	{ ControlMask,  XK_R,             t_reload_all,         None },
 	{ 0,            XK_Delete,        g_remove_image,      None },
 
-	//{ 0,            XK_n,             i_navigate,           +1 },
 	{ 0,            XK_KP_Right,      i_navigate,           +1 },
 	{ 0,            XK_space,         i_navigate,           +1 },
-	{ 0,            XK_KP_Begin,      i_navigate,           +1 },
-	{ 0,            XK_p,             i_navigate,           -1 },
-// From recent rebase. Interesting -- is this 'move to first image' in thumbnail view?
-	// { 0,            XK_p,             i_scroll_to_edge,     (DIR_LEFT | DIR_UP) },
 	{ 0,            XK_KP_Left,       i_navigate,           -1 },
 	{ 0,            XK_BackSpace,     i_navigate,           -1 },
 	{ 0,            XK_bracketright,  i_navigate,           +10 },
@@ -114,20 +107,13 @@ static const keymap_t keys[] = {
 	{ 0,            XF86XK_Save,      i_alternate,          None },
 	{ 0,            XK_braceleft,     g_change_gamma,       -1 },
 	{ 0,            XK_braceright,    g_change_gamma,       +1 },
-	{ 0,            XK_G,             g_change_gamma,        0 },
-	// this one doesn't seem to work. Does it need to be KP_4 instead of KP_Left?
+	{ ControlMask,  XK_G,             g_change_gamma,        0 },
 	{ 0          ,  XK_KP_4,          g_first,              None },
 	{ Mod1Mask   ,  XK_Left,          g_reorder_image,      -9999999 },
 	{ Mod1Mask   ,  XK_Right,         g_reorder_image,      9999999 },
-	{ Mod1Mask|ControlMask, XK_Left, g_reorder_marked_images,  -1 },
+	{ Mod1Mask|ControlMask, XK_Left,  g_reorder_marked_images,  -1 },
 	{ Mod1Mask|ControlMask, XK_Right, g_reorder_marked_images, 1 },
 	{ 0,            XK_KP_Begin,      g_n_or_last,          None },
-
-// these don't work, after the animation system change.
-//	{ ControlMask,  XK_n,             i_navigate_frame,     +1 },
-//	{ ControlMask,  XK_p,             i_navigate_frame,     -1 },
-//	{ 0          ,  XK_KP_Next,       i_navigate_frame,     +1 },
-//	{ 0          ,  XK_KP_End,        i_navigate_frame,     -1 },
 	{ ControlMask,  XK_space,         i_toggle_animation,   None },
 	{ 0,            XK_Left,          i_scroll,             DIR_LEFT },
 	{ 0,            XK_Down,          i_scroll,             DIR_DOWN },
@@ -137,15 +123,11 @@ static const keymap_t keys[] = {
 	{ 0          ,  XK_KP_Enter,      i_toggle_animation,   None },
 // marking an image should also move to the next image -- see https://github.com/muennich/sxiv/issues/172
 // Note the order of entries here is significant -- we don't want to navigate to next before the toggle is processed.
-	{ 0,            XK_m,             g_toggle_image_mark, None },
-	{ 0,            XK_m,             i_navigate,           +1 },
-	{ 0,            XK_m,             t_move_sel,           DIR_RIGHT },
-	{ ControlMask,  XK_m,             g_reverse_marks,     None },
-	{ 0,            XK_M,             g_unmark_all,     None },
-// .. but we can also use the alternate if we don't want this behaviour.
-	{ 0,            XK_KP_Divide,     g_toggle_image_mark, None },
-	{ ControlMask,  XK_KP_Multiply,   g_reverse_marks,     None },
-	{ ShiftMask,    XK_KP_Multiply,   g_unmark_all,     None },
+	{ 0,            XK_Menu,          g_toggle_image_mark, None },
+	{ 0,            XK_Menu,          i_navigate,           +1 },
+	{ 0,            XK_Menu,          t_move_sel,           DIR_RIGHT },
+	{ ControlMask,  XK_Menu,          g_reverse_marks,     None },
+	{ ShiftMask,    XK_Menu,          g_unmark_all,     None },
 	{ 0,            XK_KP_Insert,     g_navigate_marked,   -1 },
 	{ 0,            XK_KP_Delete,     g_navigate_marked,   +1 },
 	{ 0,            XK_Left,          t_move_sel,          DIR_LEFT },
@@ -160,22 +142,21 @@ static const keymap_t keys[] = {
 	{ ShiftMask,    XK_Down,          i_scroll_to_edge,     DIR_DOWN },
 	{ ShiftMask,    XK_Up,            i_scroll_to_edge,     DIR_UP },
 	{ ShiftMask,    XK_Right,         i_scroll_to_edge,     DIR_RIGHT },
-	{ 0,            XK_equal,         i_set_zoom,           100 },
-	{ 0,            XK_w,             i_fit_to_win,         SCALE_DOWN },
-	{ 0,            XK_W,             i_fit_to_win,         SCALE_FIT },
-	{ 0,            XK_e,             i_fit_to_win,         SCALE_WIDTH },
-	{ 0,            XK_E,             i_fit_to_win,         SCALE_HEIGHT },
+	{ ControlMask,  XK_plus,          i_set_zoom,           100 },
+	{ 0,            XK_minus,         i_fit_to_win,         SCALE_DOWN },
+	{ 0,            XK_underscore,    i_fit_to_win,         SCALE_FIT },
+	{ ControlMask,  XK_minus,         i_fit_to_win,         SCALE_WIDTH },
+	{ ControlMask,  XK_underscore,    i_fit_to_win,         SCALE_HEIGHT },
 	{ 0,            XK_comma,         i_rotate,             DEGREE_270 },
 	{ 0,            XK_period,        i_rotate,             DEGREE_90 },
 	{ 0,            XK_greater,       i_rotate,             DEGREE_180 },
 	{ 0,            XK_backslash,     i_flip,               FLIP_HORIZONTAL },
 	{ 0,            XK_minus,         i_flip,               FLIP_VERTICAL },
-	{ 0,            XK_s,             i_slideshow,          None },
-	// { 0,            XK_F6,            i_slideshow,          None },
+	{ ControlMask,  XK_S,             i_slideshow,          None },
 
-	{ ControlMask,  XK_F9,            i_toggle_mouse_pos,   None },
-	{ 0,            XK_F9,            i_toggle_antialias,   None },
-	{ ShiftMask,    XK_F9,            i_toggle_alpha,      None },
+	{ ControlMask,  XK_P,             i_toggle_mouse_pos,   None },
+	{ ControlMask,  XK_A,             i_toggle_antialias,   None },
+	{ ControlMask,  XK_T,             i_toggle_alpha,       None },
 
 };
 
