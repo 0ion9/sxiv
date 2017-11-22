8732a7e87adf9eca5988baff54eb6b5116f2d92f
David Gowers <finticemo@gmail.com>
2017-11-22 17:39:57 +1030

    Sort keyboard commands spatially (top -> bottom, left->right)
      A stronger zoom (a/z -> g_zoom(-2/+2)) has been bound but needs backend work to fully enable.

--- config.def.h
+++ config.def.h
@@ -111,139 +111,205 @@ static const int THUMB_SIZE = 3;
 #endif
 #ifdef _MAPPINGS_CONFIG
 
+/* alias for clarity */
+#define AltMask Mod1Mask
+
 /* keyboard mappings for image and thumbnail mode: */
 /* Plain Alphanumerics, shifted alphanumerics, control+alphanumerics and function keys are explicitly avoided,
    to allow the key handler script maximum latitude. */
+
+/* keyboard layout diagram:
+
+   ^^ f1 f2 f3 f4  f5 f6 f7 f8  f9 fA fB fC  Ps  Sl  Pa
+
+   ~ 1  2  3  4  5  6  7  8  9  0  -  =  <<  In  Ho  Pu  Nl /  *  -
+   
+   Ta q  w  f  p  g  j  l  u  y  ;  [  ]  \  De  En  Pd  7H 8^ 9U +
+                                                                  +
+   <<  a  r  s  t  d  h  n  e  i  o  ' Entr              4< 5  6> +
+
+   Shft z  x  c  v  b  k  m  ,  .  /  Shift      ^       1E 2v 3D E
+                                                                  n
+   Ctl Hy Al    S p a c e     Al Hy Me  Ctl  <   v   >   0Ins  .D t
+
+
+
+ */
+
 static const keymap_t keys[] = {
 	/* modifiers    key               function              argument */
         { ShiftMask,    XK_Escape,        g_quit,               None },
         { ShiftMask,    XF86XK_Close,     g_quit,               None },
-        { 0,            XK_Return,        g_switch_mode,        None },
-        { ControlMask,  XK_F,             g_toggle_fullscreen,  None },
-        { ControlMask,  XK_B,             g_toggle_bar,         None },
-	{ 0,            XK_KP_Add,        g_zoom,               +1 },
-	{ 0,            XK_plusminus,     g_zoom,               +1 },
-	{ 0,            XK_KP_Subtract,   g_zoom,               -1 },
-	{ 0,            XK_identical,     g_zoom,               -1 },
-	{ 0,            XK_a,             g_zoom,               +1 },
-	{ 0,            XK_z,             g_zoom,               -1 },
 
-	{ ControlMask,  XK_R,             t_reload_all,         None },
-	{ 0,            XK_Delete,        g_remove_image,      None },
 
-	{ 0,            XK_KP_Right,      i_navigate,           +1 },
-	{ 0,            XK_space,         i_navigate,           +1 },
-	{ 0,            XK_KP_Left,       i_navigate,           -1 },
-	{ 0,            XK_BackSpace,     i_navigate,           -1 },
-	{ 0,            XK_bracketright,  i_navigate,           +10 },
-	{ 0,            XK_bracketleft,   i_navigate,           -10 },
-	{ 0,            XK_rightt,        i_navigate,           +10 },
-	{ 0,            XK_leftt,         i_navigate,           -10 },
-	{ 0,            XK_KP_Prior,      i_navigate,           +10 },
-	{ 0,            XK_KP_Home,       i_navigate,           -10 },
-	{ 0,            XF86XK_Save,      i_alternate,          None },
-	{ 0,            XF86XK_Back,      i_navigate_frame,     -1 },
-	{ 0,            XF86XK_Forward,   i_navigate_frame,     1 },
-	{ 0,            XK_braceleft,     g_change_gamma,       -1 },
-	{ 0,            XK_braceright,    g_change_gamma,       +1 },
-	{ ControlMask,  XK_G,             g_change_gamma,        0 },
-	{ 0          ,  XK_S,             g_cycle_silhouetting, None },
-	{ 0          ,  XK_T,             g_cycle_opacity,      0 },
-	{ 0          ,  XK_W,             i_cycle_tiling,       None },
-	{ 0          ,  XK_X,             i_cycle_scalefactor,  None },
-	{ ControlMask,  XK_t,             g_toggle_negalpha,    None },
-	{ 0          ,  XK_KP_4,          g_first,              None },
-	{ 0          ,  XK_C,             g_clone_image,        None },
-	{ Mod1Mask   ,  XK_Left,          g_reorder_image,      -9999999 },
-	{ Mod1Mask   ,  XK_Right,         g_reorder_image,      9999999 },
-	{ ShiftMask  ,  XK_Left,          g_reorder_image,      -16 },
-	{ ShiftMask  ,  XK_Right,         g_reorder_image,      16 },
-/* XXX instead of abusing 'alternate', implement a separate 'target marker' */
-	{ Mod1Mask|ShiftMask,  XK_Left,   g_reorder_image_to_alternate,      -1 },
-	{ Mod1Mask|ShiftMask,  XK_Right,  g_reorder_image_to_alternate,      1 },
-	{ 0          ,  XK_Pointer_Left,  t_move_and_reorder,   DIR_LEFT },
-	{ Mod1Mask   ,  XK_Left,          t_move_and_reorder,   DIR_LEFT },
-	{ 0          ,  XK_Pointer_Right, t_move_and_reorder,   DIR_RIGHT },
-	{ Mod1Mask   ,  XK_Right,         t_move_and_reorder,   DIR_RIGHT },
-	{ 0          ,  XK_Pointer_Up,    t_move_and_reorder,   DIR_UP },
-	{ Mod1Mask   ,  XK_Up,            t_move_and_reorder,   DIR_UP },
-	{ 0          ,  XK_Pointer_Down,  t_move_and_reorder,   DIR_DOWN },
-	{ Mod1Mask   ,  XK_Down,          t_move_and_reorder,   DIR_DOWN },
-	{ Mod1Mask|ControlMask, XK_Left,  g_reorder_marked_images,  -1 },
-	{ Mod1Mask|ControlMask, XK_Right, g_reorder_marked_images, 1 },
-	// xxx absurd. get a less modifiery shortcut for this
-	{ ShiftMask|ControlMask, XK_Left,  g_reorder_marked_images_to_alternate,  -1 },
-	{ ShiftMask|ControlMask, XK_Right, g_reorder_marked_images_to_alternate, 1 },
-	{ 0,            XK_KP_Begin,      g_n_or_last,          None },
-	{ Mod1Mask,     XK_g,             g_n_or_last,          None },
-	{ ControlMask,  XK_space,         i_toggle_animation,   None },
-	{ 0,            XK_Left,          i_scroll,             DIR_LEFT },
-	{ 0,            XK_Down,          i_scroll,             DIR_DOWN },
-	{ 0,            XK_Up,            i_scroll,             DIR_UP },
-	{ 0,            XK_Right,         i_scroll,             DIR_RIGHT },
-	{ 0,            XK_leftarrow,     i_scroll,             DIR_LEFT },
-	{ 0,            XK_downarrow,     i_scroll,             DIR_DOWN },
-	{ 0,            XK_uparrow,       i_scroll,             DIR_UP },
-	{ 0,            XK_rightarrow,    i_scroll,             DIR_RIGHT },
-	{ 0          ,  XK_KP_Down,       i_toggle_animation,   None },
-	{ 0          ,  XK_KP_Enter,      i_toggle_animation,   None },
-	{ ControlMask,  XK_Return,        i_toggle_animation,   None },
-// marking an image should also move to the next image -- see https://github.com/muennich/sxiv/issues/172
-// Note the order of entries here is significant -- we don't want to navigate to next before the toggle is processed.
-	{ 0,            XK_Menu,          g_toggle_image_mark, None },
-	{ 0,            XK_Menu,          i_navigate,           +1 },
-	{ 0,            XK_Menu,          t_move_sel,           DIR_RIGHT },
-	{ ControlMask,  XK_Menu,          g_reverse_marks,     None },
-	{ ShiftMask,    XK_Menu,          g_unmark_all,     None },
-	{ 0,            XK_KP_Insert,     g_navigate_marked,   -1 },
-	{ 0,            XK_KP_Delete,     g_navigate_marked,   +1 },
-	{ Mod1Mask,     XK_bracketleft,   g_navigate_marked,   -1 },
-	{ Mod1Mask,     XK_bracketright,  g_navigate_marked,   +1 },
-	{ 0,            XK_Left,          t_move_sel,          DIR_LEFT },
-	{ 0,            XK_Down,          t_move_sel,          DIR_DOWN },
-	{ 0,            XK_Up,            t_move_sel,          DIR_UP },
-	{ 0,            XK_Right,         t_move_sel,          DIR_RIGHT },
-	{ 0,            XK_leftarrow,     t_move_sel,          DIR_LEFT },
-	{ 0,            XK_downarrow,     t_move_sel,          DIR_DOWN },
-	{ 0,            XK_uparrow,       t_move_sel,          DIR_UP },
-	{ 0,            XK_rightarrow,    t_move_sel,          DIR_RIGHT },
-	{ ControlMask,  XK_Left,          g_scroll_screen,     DIR_LEFT },
-	{ ControlMask,  XK_Down,          g_scroll_screen,     DIR_DOWN },
-	{ ControlMask,  XK_Up,            g_scroll_screen,     DIR_UP },
-	{ ControlMask,  XK_Right,         g_scroll_screen,     DIR_RIGHT },
-	{ ControlMask,  XK_leftarrow,     g_scroll_screen,     DIR_LEFT },
-	{ ControlMask,  XK_downarrow,     g_scroll_screen,     DIR_DOWN },
-	{ ControlMask,  XK_uparrow,       g_scroll_screen,     DIR_UP },
-	{ ControlMask,  XK_rightarrow,    g_scroll_screen,     DIR_RIGHT },
-	{ ShiftMask,    XK_Left,          i_scroll_to_edge,     DIR_LEFT },
-	{ ShiftMask,    XK_Down,          i_scroll_to_edge,     DIR_DOWN },
-	{ ShiftMask,    XK_Up,            i_scroll_to_edge,     DIR_UP },
-	{ ShiftMask,    XK_Right,         i_scroll_to_edge,     DIR_RIGHT },
-	{ ShiftMask,    XK_leftarrow,     i_scroll_to_edge,     DIR_LEFT },
-	{ ShiftMask,    XK_downarrow,     i_scroll_to_edge,     DIR_DOWN },
-	{ ShiftMask,    XK_uparrow,       i_scroll_to_edge,     DIR_UP },
-	{ ShiftMask,    XK_rightarrow,    i_scroll_to_edge,     DIR_RIGHT },
+        // 1 2 3 4 5 6 7 8 9 0 -> internal 'score' cmd, using prefix?
+        // minus
+        // dash
+	{ 0,            XK_minus,         i_flip,               FLIP_VERTICAL },
+	{ AltMask,      XK_minus,         i_fit_to_win,         SCALE_DOWN },
+	{ 0,            XK_emdash,        i_flip,               FLIP_VERTICAL },
 	{ ControlMask,  XK_plus,          i_set_zoom,           100 },
-	{ Mod1Mask,     XK_minus,         i_fit_to_win,         SCALE_DOWN },
 	{ 0,            XK_underscore,    i_fit_to_win,         SCALE_FIT },
 	{ ControlMask,  XK_minus,         i_fit_to_win,         SCALE_WIDTH },
 	{ ControlMask,  XK_underscore,    i_fit_to_win,         SCALE_HEIGHT },
+	{ AltMask,      XK_BackSpace,     i_navigate,           -1 },
+        // Ins
+        // Home
+        // PgUp
+        // NumLock
+        // Kp_Div
+        // KP_Mul
+	{ 0,            XK_KP_Subtract,   g_zoom,               -1 },
+	{ 0,            XK_identical,     g_zoom,               -1 },
+
+
+        { 0,            XK_Tab,           g_switch_mode,        None },
+        // XXX s-Tab -> fullscreen, c-Tab -> altimage
+        // Q
+	{ 0          ,  XK_W,             i_cycle_tiling,       None },
+        { ControlMask,  XK_F,             g_toggle_fullscreen,  None },
+	{ 0,            XK_P,             i_print_mouse_pos,    None },
+	{ ControlMask,  XK_P,             i_toggle_mouse_pos,   None },
+	{ AltMask,      XK_g,             g_n_or_last,          None },
+        // j
+        // l
+        // u
+        // semicolon
+	{ 0,            XK_bracketleft,   i_navigate,           -10 },
+        { 0,            XK_bracketright,  i_navigate,           +10 },
+	{ 0,            XK_leftt,         i_navigate,           -10 },
+	{ 0,            XK_rightt,        i_navigate,           +10 },
+	{ 0,            XK_braceleft,     g_change_gamma,       -1 },
+	{ 0,            XK_braceright,    g_change_gamma,       +1 },
+	{ AltMask,      XK_bracketleft,   g_navigate_marked,    -1 },
+	{ AltMask,      XK_bracketright,  g_navigate_marked,    +1 },
+	{ 0,            XK_backslash,     i_flip,               FLIP_HORIZONTAL },
+	{ 0,            XK_exclamdown,    i_flip,               FLIP_HORIZONTAL },
+	{ 0,            XK_Delete,        g_remove_image,      None },
+        // End
+        // PgDn
+        // KP_AltMask
+        // KP_Up
+	{ 0,            XK_KP_Prior,      i_navigate,           +10 },
+        // XXX thumb mode navigate in pages?
+        { 0,            XK_KP_Add,        g_zoom,               +1 },
+	{ 0,            XK_plusminus,     g_zoom,               +1 },
+
+	{ 0,            XK_a,             g_zoom,               +2 },
+	{ ControlMask,  XK_A,             i_cycle_antialias,    None },
+        // XXX with inotify, reloadall may not be needed.
+	{ ControlMask,  XK_R,             t_reload_all,         None },
+	{ 0          ,  XK_S,             g_cycle_silhouetting, None },
+	{ ControlMask,  XK_S,             i_slideshow,          None },
+	{ ControlMask,  XK_t,             g_toggle_negalpha,    None },
+	{ 0          ,  XK_T,             g_cycle_opacity,      0 },
+	{ ControlMask,  XK_T,             i_toggle_alpha,       None },
 	{ ControlMask,  XK_D,             i_fit_to_win,         SCALE_DISTORT },
+        // h         
+	{ 0,            XK_n,             i_navigate,           -1 },
+	{ 0,            XK_e,             i_navigate,           +1 },
+	{ AltMask,      XK_e,             i_alternate,          None },
+        // i
+        // o
+        // '
+        { 0,            XK_Return,        g_switch_mode,        None },
+	{ 0,            XK_KP_Left,       i_navigate,           -1 },
+	{ 0,            XK_KP_4,          g_first,              None },
+	{ 0,            XK_KP_Begin,      g_n_or_last,          None },
+	{ 0,            XK_KP_Right,      i_navigate,           +1 },
+	{ 0,            XK_z,             g_zoom,               -2 },
+        // x 
+	{ 0,            XK_x,             i_navigate_frame,     -1 },
+	{ 0          ,  XK_X,             i_cycle_scalefactor,  None },
+	{ 0,            XK_c,             i_navigate_frame,     1 },
+	{ 0          ,  XK_C,             g_clone_image,        None },
+        { ControlMask,  XK_B,             g_toggle_bar,         None },
+        // k
+        // m
+        // ,
 	{ 0,            XK_comma,         i_rotate,             DEGREE_270 },
 	{ 0,            XK_period,        i_rotate,             DEGREE_90 },
 	{ 0,            XK_greater,       i_rotate,             DEGREE_180 },
 	{ 0,            XK_kana_comma,    i_rotate,             DEGREE_270 },
 	{ 0,            XK_kana_fullstop, i_rotate,             DEGREE_90 },
 	{ 0,            XK_emopencircle,  i_rotate,             DEGREE_180 },
-	{ 0,            XK_backslash,     i_flip,               FLIP_HORIZONTAL },
-	{ 0,            XK_minus,         i_flip,               FLIP_VERTICAL },
-	{ 0,            XK_exclamdown,    i_flip,               FLIP_HORIZONTAL },
-	{ 0,            XK_emdash,        i_flip,               FLIP_VERTICAL },
-	{ ControlMask,  XK_S,             i_slideshow,          None },
-	{ 0,            XK_P,             i_print_mouse_pos,    None },
-	{ ControlMask,  XK_P,             i_toggle_mouse_pos,   None },
-	{ ControlMask,  XK_A,             i_cycle_antialias,    None },
-	{ ControlMask,  XK_T,             i_toggle_alpha,       None },
+        // KP_End?
+        // KP_PgDn?
+	{ 0          ,  XK_KP_Down,       i_toggle_animation,   None },
+	{ 0          ,  XK_KP_Enter,      i_toggle_animation,   None },
+	{ ControlMask,  XK_Return,        i_toggle_animation,   None },
+
+
+        { 0,            XK_space,         i_navigate,           +1 },
+	{ ControlMask,  XK_space,         i_toggle_animation,   None },
+	{ 0,            XK_Menu,          g_toggle_image_mark, None },
+	{ 0,            XK_Menu,          i_navigate,           +1 },
+	{ 0,            XK_Menu,          t_move_sel,           DIR_RIGHT },
+	{ ControlMask,  XK_Menu,          g_reverse_marks,     None },
+	{ ShiftMask,    XK_Menu,          g_unmark_all,     None },
+	{ 0,            XK_Left,          i_scroll,             DIR_LEFT },
+	{ 0,            XK_Left,          t_move_sel,          DIR_LEFT },
+	{ ControlMask,  XK_Left,          g_scroll_screen,     DIR_LEFT },
+	{ 0          ,  XK_Pointer_Left,  t_move_and_reorder,   DIR_LEFT },
+	{ AltMask   ,  XK_Left,          t_move_and_reorder,   DIR_LEFT },
+	{ AltMask   ,  XK_Left,          g_reorder_image,      -9999999 },
+	{ ShiftMask  ,  XK_Left,          g_reorder_image,      -16 },
+	{ ShiftMask,    XK_Left,          i_scroll_to_edge,     DIR_LEFT },
+	{ AltMask|ControlMask, XK_Left,  g_reorder_marked_images,  -1 },
+	{ AltMask|ShiftMask,  XK_Left,   g_reorder_image_to_alternate,      -1 },
+	{ ShiftMask|ControlMask, XK_Left,  g_reorder_marked_images_to_alternate,  -1 },
+	{ 0,            XK_Down,          i_scroll,             DIR_DOWN },
+	{ 0,            XK_Up,            i_scroll,             DIR_UP },
+	{ 0,            XK_Pointer_Up,    t_move_and_reorder,   DIR_UP },
+	{ AltMask,      XK_Up,            t_move_and_reorder,   DIR_UP },
+	{ 0,            XK_Right,         i_scroll,             DIR_RIGHT },
+	{ 0,            XK_Right,         t_move_sel,          DIR_RIGHT },
+	{ ControlMask,  XK_Right,         g_scroll_screen,     DIR_RIGHT },
+	{ ShiftMask,    XK_Right,         i_scroll_to_edge,     DIR_RIGHT },
+	{ 0,            XK_Pointer_Right, t_move_and_reorder,   DIR_RIGHT }, 
+	{ AltMask,      XK_Right,         t_move_and_reorder,   DIR_RIGHT },
+	{ AltMask,      XK_Right,         g_reorder_image,      9999999 },
+	{ ShiftMask,    XK_Right,         g_reorder_image,      16 },
+/* XXX instead of abusing 'alternate', implement a separate 'target marker' */
+	{ AltMask|ShiftMask,  XK_Right,  g_reorder_image_to_alternate,      1 },
+	{ AltMask|ControlMask, XK_Right, g_reorder_marked_images, 1 },
+	// xxx absurd. get a less modifiery shortcut for this
+	{ ShiftMask|ControlMask, XK_Right, g_reorder_marked_images_to_alternate, 1 },
+
+	{ 0,            XK_Down,          t_move_sel,          DIR_DOWN },
+	{ ControlMask,  XK_Down,          g_scroll_screen,     DIR_DOWN },
+	{ ShiftMask,    XK_Down,          i_scroll_to_edge,     DIR_DOWN },
+	{ 0,            XK_downarrow,     t_move_sel,          DIR_DOWN },
+	{ ControlMask,  XK_downarrow,     g_scroll_screen,     DIR_DOWN },
+	{ ShiftMask,    XK_downarrow,     i_scroll_to_edge,     DIR_DOWN },
+	{ 0,            XK_Up,            t_move_sel,          DIR_UP },
+	{ ControlMask,  XK_Up,            g_scroll_screen,     DIR_UP },
+	{ ShiftMask,    XK_Up,            i_scroll_to_edge,     DIR_UP },
+	{ 0,            XK_uparrow,       t_move_sel,          DIR_UP },
+	{ ControlMask,  XK_uparrow,       g_scroll_screen,     DIR_UP },
+	{ ShiftMask,    XK_uparrow,       i_scroll_to_edge,     DIR_UP },
+
+	{ 0,            XK_leftarrow,     i_scroll,             DIR_LEFT },
+	{ 0,            XK_leftarrow,     t_move_sel,          DIR_LEFT },
+	{ ControlMask,  XK_leftarrow,     g_scroll_screen,     DIR_LEFT },
+	{ ShiftMask,    XK_leftarrow,     i_scroll_to_edge,     DIR_LEFT },
+	{ 0,            XK_downarrow,     i_scroll,             DIR_DOWN },
+	{ 0,            XK_uparrow,       i_scroll,             DIR_UP },
+	{ 0,            XK_rightarrow,    i_scroll,             DIR_RIGHT },
+	{ ControlMask,  XK_rightarrow,    g_scroll_screen,     DIR_RIGHT },
+	{ ShiftMask,    XK_rightarrow,    i_scroll_to_edge,     DIR_RIGHT },
+	{ 0,            XK_rightarrow,    t_move_sel,          DIR_RIGHT },
+	{ 0,            XK_Pointer_Down,  t_move_and_reorder,   DIR_DOWN },
+	{ AltMask,      XK_Down,          t_move_and_reorder,   DIR_DOWN },
+
+	{ 0,            XK_KP_Insert,     g_navigate_marked,   -1 },
+	{ 0,            XK_KP_Delete,     g_navigate_marked,   +1 },
+
+//////
+	{ 0,            XK_KP_Next,       i_navigate,           -10 }
+
+// marking an image should also move to the next image -- see https://github.com/muennich/sxiv/issues/172
+// Note the order of entries here is significant -- we don't want to navigate to next before the toggle is processed.
 
 };
 
